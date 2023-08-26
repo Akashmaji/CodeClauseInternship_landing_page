1b35d0c0375e70b919721af9e38c93df90a74bd1
Akash-Maji <akashmaji556@gmail.com>
2023-08-26 13:59:09 +0530

    first commit

---
+++ src/common/header/Search.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import logo from "../../components/assets/images/logo.svg"
+import { Link } from "react-router-dom"
+
+const Search = ({ CartItem }) => {
+  // fixed Header
+  window.addEventListener("scroll", function () {
+    const search = document.querySelector(".search")
+    search.classList.toggle("active", window.scrollY > 100)
+  })
+
+  return (
+    <>
+      <section className='search'>
+        <div className='container c_flex'>
+          <div className='logo'>
+            <img src={logo} alt='' height={20} width={48}/>
+          </div>
+
+          <div className='search-box f_flex'>
+            <i className='fa fa-search'></i>
+            <input type='text' placeholder='Search and hit enter...' />
+            <span>All Category</span>
+          </div>
+
+          <div className='icon f_flex width'>
+            <i className='fa fa-user icon-circle'></i>
+            
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Search
